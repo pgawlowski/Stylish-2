08e6b14a0ac6f1ca98d4a7c30e7fcbe87443a180
Piotr Gawlowski <piotr.gawlowski@sap.com>
2017-05-15 09:14:36 +0200

    Bundle fix

--- Stylish-2/Styleable/Stylesheet.swift
+++ Stylish-2/Styleable/Stylesheet.swift
@@ -71,7 +71,7 @@ class JSONStylesheet : Stylesheet {
         let paths = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
         let savedDirectory = paths[0]
         let filename = savedDirectory.appending("/stylesheet.json")
-        let bundle = Bundle.main
+        let bundle = BundleMarker()
         let fileManager = FileManager.default
         
         
